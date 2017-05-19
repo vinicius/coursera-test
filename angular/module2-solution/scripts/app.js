f851566af9014868ec87d79b339b6ae5625ca862
Vinicius Pinheiro <vgpinheiro@tecgraf.puc-rio.br>
2017-05-19 03:08:09 -0300

    Solução para o exercício da semana 2 do curso AngularJS.

---
+++ angular/module2-solution/scripts/app.js
@@ -0,0 +1,52 @@
+(function() {
+    'use strict';
+
+    angular.module('ShoppingListCheckOff', [])
+    .controller('ToBuyController', ToBuyController)
+    .controller('AlreadyBoughtController', AlreadyBoughtController)
+    .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+    ToBuyController.$inject = ['ShoppingListCheckOffService'];
+    function ToBuyController(ShoppingListCheckOffService) {
+        var toBuyController = this;
+        toBuyController.toBuyList = ShoppingListCheckOffService.getToBuyList();
+        toBuyController.bought = function(index) {
+            console.log(index);
+            ShoppingListCheckOffService.buyItem(index);
+        }
+    }
+
+    ToBuyController.$inject = ['ShoppingListCheckOffService'];
+    function AlreadyBoughtController(ShoppingListCheckOffService) {
+        var alreadyBoughtController = this;
+        alreadyBoughtController.boughtList = ShoppingListCheckOffService.getBoughtList();
+    }
+
+    function ShoppingListCheckOffService() {
+	    var service = this;
+
+	    var toBuyItems = [{ name: "cookies", quantity: 10 },
+                          { name: "tomates", quantity: 8 },
+                          { name: "bananas", quantity: 3 },
+                          { name: "cervejas", quantity: 5 },
+                          { name: "lasanhas", quantity: 12 }];
+
+	    var boughtItems = [];
+
+	    service.getToBuyList = function () {
+            return toBuyItems;
+        };
+
+
+	    service.getBoughtList = function() {
+            return boughtItems;
+	    }
+
+	    service.buyItem = function(index) {
+            boughtItems.push(toBuyItems[index]);
+            if (index > -1) {
+                toBuyItems.splice(index, 1);
+            }
+        }
+    }
+})();
